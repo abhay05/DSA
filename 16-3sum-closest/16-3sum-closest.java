7e51c89141bfd727cfc613d8c29eebec6d8433e6
abhay05 <abhaypsc.5@gmail.com>
2022-01-29 20:23:44 +0530

    Time: 6 ms (70.90%), Space: 43.7 MB (5.91%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.java
+++ 16-3sum-closest/16-3sum-closest.java
@@ -4,52 +4,28 @@ class Solution {
         int minSum=1000000000;
         int ans=0;
         for(int i=0;i<nums.length-2;i++){
-            for(int j=i+1;j<nums.length-1;j++){
-                int a=target-(nums[i]+nums[j]);
-                int l=j+1;
-                int u=nums.length-1;
-                while(l<=u){
-                    int mid=(l+u)/2;
-                    
-                     // System.out.println(minSum);
-                     // System.out.println(ans);
-                    // System.out.println(u);
-                    // System.out.println(a);
-                    if(nums[mid]<a){
-                        l=mid+1;
-                        if(minSum>a-nums[mid]){
-                          minSum=Math.min(minSum,a-nums[mid]);
-                            ans=nums[i]+nums[j]+nums[mid];
-                        }
-                        if(mid+1<nums.length && Math.abs(nums[mid+1]-a)<Math.abs(nums[mid]-a)){
-                            
-                            if(minSum>Math.abs(nums[mid+1]-a)){
-                          minSum=Math.min(minSum,Math.abs(nums[mid+1]-a));
-                                ans=nums[i]+nums[j]+nums[mid+1];
-                        }
-                        }
-                    }else if(nums[mid]>a){
-                        
-                        if(minSum>nums[mid]-a){
-                          minSum=Math.min(minSum,nums[mid]-a);
-                            ans=nums[i]+nums[j]+nums[mid];
-                        }
-                        if(mid-1>=l && Math.abs(nums[mid-1]-a)<Math.abs(nums[mid]-a)){
-                            
-                                if(minSum>Math.abs(nums[mid-1]-a)){
-                          minSum=Math.min(minSum,Math.abs(nums[mid-1]-a));
-                                ans=nums[i]+nums[j]+nums[mid-1];
-                        }
-                            
-                        }
-                        // System.out.println("Bigger");
-                        // System.out.println(ans);
-                        // System.out.println(minSum);
-                        u=mid-1;
-                    }else{
-                        return target;
-                    }
+            int j=i+1;
+            int k=nums.length-1;
+            int val=target-nums[i];
+            while(j<k){
+                
+                if(val-(nums[j]+nums[k])>0){
+                    if(minSum>Math.abs(val-(nums[j]+nums[k]))){
+                    minSum=Math.abs(val-(nums[j]+nums[k]));
+                    ans=nums[i]+nums[j]+nums[k];
                 }
+                    j++;
+                }else if(val-(nums[j]+nums[k])<0){
+                    if(minSum>Math.abs(val-(nums[j]+nums[k]))){
+                    minSum=Math.abs(val-(nums[j]+nums[k]));
+                    ans=nums[i]+nums[j]+nums[k];
+                }
+                    k--;
+                }else{
+                    return nums[i]+nums[j]+nums[k];
+                }
+                
+                
             }
         }
         return ans;
