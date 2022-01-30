624dfaf00975d0918b0eca729887605106bb0e54
abhay05 <abhaypsc.5@gmail.com>
2022-01-30 20:18:04 +0530

    Time: 8 ms (41.56%), Space: 47.7 MB (5.23%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.java
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -7,7 +7,8 @@ class Solution {
         for(int i=0;i<9;i++){
             HashMap<Character,Integer> temp1=new HashMap<Character,Integer>();
             for(int j=0;j<9;j++){
-                if(board[i][j]!='.' && temp1.get(board[i][j]) !=null){
+                if(board[i][j]=='.')continue;
+                if(temp1.get(board[i][j]) !=null){
                     return false;
                 }
                 temp1.put(board[i][j],1);
@@ -18,7 +19,8 @@ class Solution {
         for(int i=0;i<9;i++){
             HashMap<Character,Integer> temp1=new HashMap<Character,Integer>();
             for(int j=0;j<9;j++){
-                if(board[j][i]!='.' && temp1.get(board[j][i]) !=null){
+                if(board[j][i]=='.')continue;
+                if( temp1.get(board[j][i]) !=null){
                     return false;
                 }
                 temp1.put(board[j][i],1);
@@ -33,7 +35,8 @@ class Solution {
                  HashMap<Character,Integer> temp1=new HashMap<Character,Integer>();
                 for(int k=0;k<3;k++){
                     for(int l=0;l<3;l++){
-                        if(board[k+i*3][l+j*3]!='.' && temp1.get(board[k+i*3][l+j*3]) !=null){
+                        if(board[k+i*3][l+j*3]=='.')continue;
+                        if(temp1.get(board[k+i*3][l+j*3]) !=null){
                             return false;
                         }
                         temp1.put(board[k+i*3][l+j*3],1);
