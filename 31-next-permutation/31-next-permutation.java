b230a1ded0a002afc66335d522c99d11cf89a65c
abhay05 <abhaypsc.5@gmail.com>
2022-01-30 06:35:44 +0530

    Time: 0 ms (100.00%), Space: 42 MB (5.58%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -7,36 +7,25 @@ class Solution {
         int maxValue=nums[n-1];
         for(int i=n-2;i>=0;i--){
             int val=nums[i];
-            int minMax=1000000000;
-            
-            if(maxValue<val){maxValue=val;continue;}
-            Arrays.sort(nums,i+1,n);
-            int l=i+1;
-            int u=n-1;
-            while(l<=u){
-                int mid=(l+u)/2;
-                if(nums[mid]>nums[i]){
-                    u=mid-1;
-                    
-                    if(nums[mid-1]<=nums[i]){
-                        ind=mid;break;
-                    }
-                }else if(nums[mid]<=nums[i]){
-                    l=mid+1;
-                }
-            }
-            if(ind!=-1){
-                int temp=nums[i];
-                nums[i]=nums[ind];
-                nums[ind]=temp;
-                break;
-            }
-            
+            if(nums[i]<nums[i+1]){st=i;break;}   
         }
-        if(ind==-1){
-  System.out.println("hh");
-              int i=0;
-            int j=n-1;
+        int minVal=1000000000;
+        int j=st+1;
+        while(j<n){
+            if(nums[j]>nums[st] && minVal>=nums[j]){
+                minVal=nums[j];
+                ind=j;
+            }
+            j++;
+        }
+        if(ind!=-1){
+                int temp=nums[st];
+                nums[st]=nums[ind];
+                nums[ind]=temp;
+                st=st+1;
+            }
+              int i=st;
+             j=n-1;
             while(i<j){
                 int temp=nums[i];
                 nums[i]=nums[j];
@@ -44,6 +33,6 @@ class Solution {
                 i++;j--;
             }
             
-        }
+        
     }
 }
