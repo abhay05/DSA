45b0469a1e1a8f06e91159a081899b0124b69514
abhay05 <abhaypsc.5@gmail.com>
2022-02-02 00:07:37 +0530

    Time: 1 ms (89.24%), Space: 48 MB (5.05%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
@@ -5,9 +5,9 @@ class Solution {
         for(i=0;i<nums.length;){
             while(ind<nums.length && nums[i]==nums[ind])ind++;
             if(i<nums.length-1 && ind<nums.length){
-            int temp=nums[i+1];
+        
             nums[i+1]=nums[ind];
-            nums[ind]=temp;}
+            }
             i++;ind++;
             if(ind>nums.length )break;
         }
