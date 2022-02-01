1eb61acc6850516cd863303388a666131b530234
abhay05 <abhaypsc.5@gmail.com>
2022-02-01 20:51:59 +0530

    Time: 2 ms (72.01%), Space: 49.5 MB (5.10%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,22 +1,16 @@
 class Solution {
-    int[] dp = new int[10002];
-    int recur(int[] nums, int ind,int n){
-        if(ind>=n-1){
-            return 0;
-        }
-        if(dp[ind]!=-1)return dp[ind];
-        int ans=1000000000;
-        for(int i=1;i<=nums[ind] && ind+i<n;i++){
-            ans=Math.min(ans,recur(nums,ind+i,n));
-        }
-        dp[ind]=ans+1;
-        return ans+1;
-    }
-    
     public int jump(int[] nums) {
-        for(int i=0;i<=nums.length;i++){
-            dp[i]=-1;
+        int ans=0;
+        int curr=0;
+        int maxL=0;
+        for(int i=0;i<nums.length;i++){
+            if(i>curr){
+                curr=maxL;
+                ans++;
+            }
+            maxL=Math.max(maxL,i+nums[i]);
+        
         }
-        return recur(nums,0,nums.length);
+        return ans;
     }
 }
