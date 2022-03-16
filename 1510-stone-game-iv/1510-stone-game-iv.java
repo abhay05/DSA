d9ef349a578d5c0e29f91306da68543931a06f1b
abhay05 <abhaypsc.5@gmail.com>
2022-03-16 20:01:12 +0530

    Time: 1718 ms (5.03%), Space: 212.3 MB (5.00%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -0,0 +1,21 @@
+class Solution {
+    HashMap<Integer,Integer>mp=new HashMap<Integer,Integer>();
+    int findWinner(int n,int p){
+        if(n==0){
+            return 1-p;
+        }
+        if(mp.containsKey(n*2+p))return mp.get(n*2+p);
+        for(int i=1;i*i<=n;i++){
+            if((1-(p^findWinner(n-i*i,1-p)))>=1){
+                mp.put(n*2+p,p);
+                return p;
+            }
+        }
+        mp.put(n*2+p,1-p);
+        return 1-p;
+    }
+    
+    public boolean winnerSquareGame(int n) {
+        return findWinner(n,1)==1;
+    }
+}
