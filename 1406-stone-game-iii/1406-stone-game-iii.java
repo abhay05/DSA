3b5f9c7c454053407c738806da8f7f054331ae13
abhay05 <abhaypsc.5@gmail.com>
2022-03-19 13:39:34 +0530

    Time: 5 ms (99.02%), Space: 51.3 MB (91.02%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -6,17 +6,16 @@
 
 class Solution {
     
-    public String stoneGameIII(int[] stoneValue) {
-        int n=stoneValue.length;
-        int [] arr=stoneValue;
+    public String stoneGameIII(int[] arr) {
+        int n=arr.length;
         int[] dp= new int[n+4];
         dp[n]=0;
         dp[n+2]=0;
         dp[n+1]=0;
         dp[n-1]=-1;
-        for(int i=n-2;i>=0;i--){
-            dp[i]=-1;
-        }
+        // for(int i=n-2;i>=0;i--){
+        //     dp[i]=-1;
+        // }
         for(int ind=n-1;ind>=0;ind--){
             int x1=ind+1<=n?arr[ind]-dp[ind+1]:-1001;
             int x2=ind+2<=n?arr[ind]+arr[ind+1]-dp[ind+2]:-1001;
