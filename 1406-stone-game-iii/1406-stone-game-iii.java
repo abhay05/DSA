92aa4c29008c1fe70702f7fed881eb9a4645916a
abhay05 <abhaypsc.5@gmail.com>
2022-03-19 07:04:51 +0530

    Time: 71 ms (34.77%), Space: 128.4 MB (8.99%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -1,16 +1,17 @@
 class Solution {
-    HashMap<Integer,Integer>mp=new HashMap<Integer,Integer>();
+   // HashMap<Integer,Integer>mp=new HashMap<Integer,Integer>();
+    int[] dp= new int[50002];
     int recur(int[] arr,int[] cumsum,int n,int ind){
         if(ind>=n){
             return -1001;
         }
-        if(mp.containsKey(ind))return mp.get(ind);
+        if(dp[ind]!=-1)return dp[ind];
         
         int x1=ind+1<=n?arr[ind]+cumsum[ind+1]-recur(arr,cumsum,n,ind+1):-1001;
         int x2=ind+2<=n?arr[ind]+arr[ind+1]+cumsum[ind+2]-recur(arr,cumsum,n,ind+2):-1001;
         int x3=ind+3<=n?arr[ind]+arr[ind+1]+arr[ind+2]+cumsum[ind+3]-recur(arr,cumsum,n,ind+3):-1001;
         int ans=Math.max(x1,Math.max(x2,x3));
-        mp.put(ind,ans);
+        dp[ind]=ans;
         return ans;
     }
     
@@ -21,8 +22,11 @@ class Solution {
         cumsum[n+1]=-1001;
         cumsum[n+2]=-1001;
         cumsum[n-1]=stoneValue[n-1];
+        dp[n]=-1;
+        dp[n-1]=-1;
         for(int i=n-2;i>=0;i--){
             cumsum[i]=(cumsum[i+1]+stoneValue[i]);
+            dp[i]=-1;
         }
         
     int val=recur(stoneValue,cumsum,stoneValue.length,0);
