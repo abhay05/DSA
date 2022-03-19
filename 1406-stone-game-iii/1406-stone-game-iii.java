74fe9838e354fd55de3ae1d00aefa5fc1ef1b902
abhay05 <abhaypsc.5@gmail.com>
2022-03-19 13:28:39 +0530

    Time: 743 ms (5.16%), Space: 422.8 MB (5.36%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -1,31 +1,36 @@
 class Solution {
+    HashMap<Integer,Integer>mp=new HashMap<Integer,Integer>();
+    int recur(int[] arr,int n,int ind,int p){
+        if(ind>=n){
+            return -1001;
+        }
+        if(mp.containsKey(ind*2+p)){
+            return mp.get(ind*2+p);
+        }
+        int x1=ind+1<=n?recur(arr,n,ind+1,1-p):-1001;
+        int x2=ind+2<=n?recur(arr,n,ind+2,1-p):-1001;
+        int x3=ind+3<=n?recur(arr,n,ind+3,1-p):-1001;
+       // System.out.println(x1+" "+x2+" "+x3+" "+ind);
+        int val1=ind+1<=n?(p==1?arr[ind+0]:-1001):-1001;
+        int val2=ind+2<=n?(p==1?arr[ind+0]+arr[ind+1]:-1001):-1001;
+        int val3=ind+3<=n?(p==1?arr[ind+0]+arr[ind+1]+arr[ind+2]:-1001):-1001;
+         if(p==1){
+             int ans = Math.max(x1+val1,Math.max(x2+val2,x3+val3));
+             mp.put(ind*2+p,ans);
+             return ans;
+         }
+        else{
+            int ans=Math.min(x1,Math.min(x2,x3));
+            mp.put(ind*2+p,ans);
+            return ans;
+            }
+    }
     
     public String stoneGameIII(int[] stoneValue) {
-        int n=stoneValue.length;
-        int [] arr=stoneValue;
-        int [] cumsum = new int[n+3];
-        int[] dp= new int[n+4];
-        cumsum[n]=-1001;
-        cumsum[n+1]=-1001;
-        cumsum[n+2]=-1001;
-        cumsum[n-1]=stoneValue[n-1];
-        dp[n]=-1001;
-        dp[n+2]=-1001;
-        dp[n+1]=-1001;
-        dp[n-1]=-1;
-        for(int i=n-2;i>=0;i--){
-            cumsum[i]=(cumsum[i+1]+stoneValue[i]);
-            dp[i]=-1;
-        }
-        for(int ind=n-1;ind>=0;ind--){
-            int x1=ind+1<=n?arr[ind]+cumsum[ind+1]-dp[ind+1]:-1001;
-            int x2=ind+2<=n?arr[ind]+arr[ind+1]+cumsum[ind+2]-dp[ind+2]:-1001;
-            int x3=ind+3<=n?arr[ind]+arr[ind+1]+arr[ind+2]+cumsum[ind+3]-dp[ind+3]:-1001;
-            int ans=Math.max(x1,Math.max(x2,x3));
-            dp[ind]=ans;
-        }
-        int val=dp[0];
+    int val=recur(stoneValue,stoneValue.length,0,1);
         int total=0;
+        val=val+1001;
+        //System.out.println("ans "+val);
         for(int i=0;i<stoneValue.length;i++){
             total+=stoneValue[i];
         }
