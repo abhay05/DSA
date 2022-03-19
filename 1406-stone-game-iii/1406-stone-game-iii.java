a8c32ae883debb8da93d2a03d26691ab57f5c44a
abhay05 <abhaypsc.5@gmail.com>
2022-03-19 13:45:32 +0530

    Time: 35 ms (53.13%), Space: 117.8 MB (60.55%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -8,20 +8,20 @@ class Solution {
     
     public String stoneGameIII(int[] arr) {
         int n=arr.length;
-        int[] dp= new int[n+4];
-        dp[n]=0;
-        dp[n+2]=0;
-        dp[n+1]=0;
-        dp[n-1]=-1;
+        int[] dp= new int[4];
+        dp[0]=0;
+        dp[2]=0;
+        dp[1]=0;
+        dp[3]=0;
         // for(int i=n-2;i>=0;i--){
         //     dp[i]=-1;
         // }
         for(int ind=n-1;ind>=0;ind--){
-            int x1=ind+1<=n?arr[ind]-dp[ind+1]:-1001;
-            int x2=ind+2<=n?arr[ind]+arr[ind+1]-dp[ind+2]:-1001;
-            int x3=ind+3<=n?arr[ind]+arr[ind+1]+arr[ind+2]-dp[ind+3]:-1001;
+            int x1=ind+1<=n?arr[ind]-dp[(ind+1)%4]:-1001;
+            int x2=ind+2<=n?arr[ind]+arr[ind+1]-dp[(ind+2)%4]:-1001;
+            int x3=ind+3<=n?arr[ind]+arr[ind+1]+arr[ind+2]-dp[(ind+3)%4]:-1001;
             int ans=Math.max(x1,Math.max(x2,x3));
-            dp[ind]=ans;
+            dp[ind%4]=ans;
         }
         int val=dp[0];
         if(val>0){
