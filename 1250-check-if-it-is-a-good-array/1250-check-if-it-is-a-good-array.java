9e64593cb0e36cda457292c00e213e6348d7146f
abhay05 <abhaypsc.5@gmail.com>
2022-03-21 22:50:11 +0530

    Time: 4 ms (56.02%), Space: 59.4 MB (57.41%) - LeetHub

--- 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.java
+++ 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.java
@@ -8,9 +8,8 @@ class Solution {
     }
     
     public boolean isGoodArray(int[] nums) {
-        Arrays.sort(nums);
-        if(nums[0]==1)return true;
         int a=nums[0];
+        if(a==1)return true;
         for(int i=1;i<nums.length;i++){
            int g=calGCD(nums[i],a);
             if(g==1)return true;
