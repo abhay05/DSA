71297413ec45aff4a230f624155db28f1c2f0956
abhay05 <abhaypsc.5@gmail.com>
2022-03-25 20:32:33 +0530

    Time: 5 ms (12.40%), Space: 43.7 MB (5.24%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.java
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -18,7 +18,20 @@ class Solution {
     }
     
     public int twoCitySchedCost(int[][] costs) {
-        int ans=recur(costs,0,0,0);
-        return ans;
+      //  int ans=recur(costs,0,0,0);
+        int[][] dp=new int[costs.length+1][costs.length+1];
+        int n=costs.length/2;
+        dp[0][0]=0;
+        for(int i=1;i<n+1;i++){
+        dp[i][0]=dp[i-1][0]+costs[i-1][0];
+        dp[0][i]=dp[0][i-1]+costs[i-1][1];
+        }
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                dp[i][j]=Math.min(dp[i-1][j]+costs[i+j-1][0],dp[i][j-1]+costs[i+j-1][1]);
+            }
+        }
+        return dp[n][n];
     }
 }
