8cce96148537488cbcc8aaad54be5c640ba22c29
abhay05 <abhaypsc.5@gmail.com>
2022-03-25 10:11:06 +0530

    Time: 2 ms (99.94%), Space: 50.1 MB (84.91%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.java
+++ 997-find-the-town-judge/997-find-the-town-judge.java
@@ -1,27 +1,15 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        int[][] map=new int[n+1][n+1];
+        int[] map=new int[n+1];
+        for(int i=0;i<n+1;i++){
+            map[i]=0;
+        }
         for(int i=0;i<trust.length;i++){
-            map[trust[i][1]][trust[i][0]]=1;
+            map[trust[i][1]]++;
+            map[trust[i][0]]--;
         }
         for(int i=1;i<=n;i++){
-            int cnt=0;
-            for(int j=1;j<=n;j++){
-                if(map[i][j]==1){
-                    cnt++;
-                }
-            }
-            if(cnt==n-1){
-                int ind=i;
-                int cnt1=0;
-                for(int k=1;k<=n;k++){
-                    if(map[k][ind]==1){
-                    cnt1++;
-                }
-                }
-                if(cnt1==0){
-                return i;}
-            }
+           if(map[i]==n-1)return i;
         }
         return -1;
     }
