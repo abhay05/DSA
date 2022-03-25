f06dc8801a647e9bfc2f364fdbab595215020826
abhay05 <abhaypsc.5@gmail.com>
2022-03-25 05:55:35 +0530

    Time: 8 ms (13.35%), Space: 57.6 MB (86.43%) - LeetHub

---
+++ 2202-maximize-the-topmost-element-after-k-moves/2202-maximize-the-topmost-element-after-k-moves.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maximumTop(int[] nums, int k) {
+        int n=nums.length;
+        int ans=-1;
+        int cnt=0;
+        for(int i=0;i<k-1&&i<n;i++){
+            ans=Math.max(ans,nums[i]);
+            cnt++;
+        }
+       // if(ans==-1)return -1;
+       // int  val=(k<=n?nums[k-1]:-1);
+        if(nums.length==1){
+            if(k%2==1){return -1;}
+            else{return nums[0];}
+        }
+        int val1=(k+1<=n?nums[k]:-1);
+        System.out.println(ans+" "+val1);
+        return Math.max(ans,val1);
+    }
+}
