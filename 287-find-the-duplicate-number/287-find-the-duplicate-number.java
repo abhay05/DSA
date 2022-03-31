10e16a70db49e83c2d48c686fd2e4a24e636b563
abhay05 <abhaypsc.5@gmail.com>
2022-03-31 08:39:45 +0530

    Time: 449 ms (5.11%), Space: 129.4 MB (5.00%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,25 +1,54 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        int l=1;
-        int u=nums.length-1;
-        int ans=-1;
-        while(l<u){
-            int mid=(l+u)/2;
-            int cnt=0;
-            for(int i=0;i<nums.length;i++){
-                if(nums[i]<=mid){
-                    cnt++;
-                }
+       int n=nums.length;
+        
+       int[][] bits=new int[n][31];
+        int[] sum=new int[31];
+        int[][] bits1=new int[n][31];
+        int[] sum1=new int[31];
+        for(int i=0;i<n;i++){
+            int temp=nums[i];
+            int ind=0;
+            while(temp!=0){
+                bits[i][ind]=temp%2;
+                temp/=2;
+                ind++;
             }
-            if(cnt>mid){
-                u=mid;
-            }
-            else{
-                l=mid+1;
-            }
-            
-            
         }
-        return l;
+        
+        for(int i=0;i<31;i++){
+            int x=0;
+            for(int ind=0;ind<n;ind++){
+                x+=bits[ind][i];   
+            }
+            sum[i]=x;
+        }
+        
+        
+        for(int i=1;i<=n-1;i++){
+            int temp=i;
+            int ind=0;
+            while(temp!=0){
+                bits1[i][ind]=temp%2;
+                temp/=2;
+                ind++;
+            }
+        }
+        
+        for(int i=0;i<31;i++){
+            int x=0;
+            for(int ind=1;ind<n;ind++){
+                x+=bits1[ind][i];   
+            }
+            sum1[i]=x;
+        }
+        
+        int ans=0;
+        for(int ind=0;ind<31;ind++){
+            if(sum[ind]>sum1[ind]){
+                ans+=(1<<ind);
+            }
+        }
+        return ans;
     }
 }
