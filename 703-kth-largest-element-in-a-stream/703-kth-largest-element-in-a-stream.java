0761b915ec2b96ecdb0e9b5a8a956b6e1fa5fcca
abhay05 <abhaypsc.5@gmail.com>
2022-04-09 08:01:22 +0530

    Time: 31 ms (16.30%), Space: 56.8 MB (9.85%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
@@ -2,6 +2,20 @@ class KthLargest {
     List<Integer> arlist=new ArrayList<>();
     int ind=0;
     int k;
+    public int find(List<Integer>list,int val){
+        int l=0;
+        int u=list.size()-1;
+        while(l<=u){
+            int mid=(l+u)/2;
+            if(list.get(mid)<=val){
+                l=mid+1;
+            }
+            else{
+                u=mid-1;
+            }
+        }
+        return l;
+    }
     public KthLargest(int k, int[] nums) {
         Arrays.sort(nums);
         this.k=k;
@@ -13,12 +27,14 @@ class KthLargest {
     }
     
     public int add(int val) {
-        for(int i=0;i<this.arlist.size();i++){
-                if(val<=this.arlist.get(i)){
-                    this.arlist.add(i,val);
-                    break;
-                }
-            }
+        int indi=find(this.arlist,val);
+        this.arlist.add(indi,val);
+        // for(int i=0;i<this.arlist.size();i++){
+        //         if(val<=this.arlist.get(i)){
+        //             this.arlist.add(i,val);
+        //             break;
+        //         }
+        //     }
             if(this.arlist.size()==0||val>this.arlist.get(this.arlist.size()-1)){
                 this.arlist.add(val);
             }
