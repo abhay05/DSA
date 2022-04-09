ca4d591e013e281a1920bb3db24de6c71833e7a1
abhay05 <abhaypsc.5@gmail.com>
2022-04-09 08:28:34 +0530

    Time: 32 ms (15.07%), Space: 52.4 MB (57.95%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
@@ -1,45 +1,21 @@
 class KthLargest {
-    List<Integer> arlist=new ArrayList<>();
-    int ind=0;
+    PriorityQueue<Integer> pq=new PriorityQueue<Integer>();
     int k;
-    public int find(List<Integer>list,int val){
-        int l=0;
-        int u=list.size()-1;
-        while(l<=u){
-            int mid=(l+u)/2;
-            if(list.get(mid)<=val){
-                l=mid+1;
-            }
-            else{
-                u=mid-1;
-            }
-        }
-        return l;
-    }
     public KthLargest(int k, int[] nums) {
-        Arrays.sort(nums);
         this.k=k;
-        for(int i=0;i<nums.length;i++){
-            this.arlist.add(nums[i]);
+        for(int a:nums){
+            add(a);
         }
-        
-        this.ind=this.arlist.size()-k;
     }
     
     public int add(int val) {
-        int indi=find(this.arlist,val);
-        this.arlist.add(indi,val);
-        // for(int i=0;i<this.arlist.size();i++){
-        //         if(val<=this.arlist.get(i)){
-        //             this.arlist.add(i,val);
-        //             break;
-        //         }
-        //     }
-            if(this.arlist.size()==0||val>this.arlist.get(this.arlist.size()-1)){
-                this.arlist.add(val);
-            }
-        return this.arlist.get(this.arlist.size()-this.k);
-        
+        if(this.pq.size()<this.k){
+            this.pq.offer(val);
+        }else if(this.pq.peek()<val){
+            this.pq.poll();
+            this.pq.offer(val);
+        }
+        return this.pq.peek();
     }
 }
 
