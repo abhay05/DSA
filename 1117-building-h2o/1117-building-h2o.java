db7d076621f311777a5ea55926142d42978073c6
abhay05 <abhaypsc.5@gmail.com>
2022-04-18 17:41:42 +0530

    Time: 16 ms (78.69%), Space: 43.7 MB (78.37%) - LeetHub

---
+++ 1117-building-h2o/1117-building-h2o.java
@@ -0,0 +1,35 @@
+class H2O {
+    Semaphore s1;
+    Semaphore s2;
+    AtomicInteger a;
+    int cnt=0;
+    public H2O() {
+        s1=new Semaphore(0);
+        s2=new Semaphore(0);
+        a=new AtomicInteger(0);
+        s1.release();
+    }
+
+    public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+		
+        // releaseHydrogen.run() outputs "H". Do not change or remove this line.
+        s1.acquire();
+        releaseHydrogen.run();
+        a.incrementAndGet();
+        if(a.get()%2==0){
+            cnt=0;
+            s2.release();
+        }else{
+        s1.release();}
+       // releaseHydrogen.run();
+       // s2.release();
+    }
+
+    public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+        
+        // releaseOxygen.run() outputs "O". Do not change or remove this line.
+		s2.acquire();
+        releaseOxygen.run();
+        s1.release();
+    }
+}
