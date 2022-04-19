5f63187549fdab0e1385c461804d11033f532e8c
abhay05 <abhaypsc.5@gmail.com>
2022-04-19 18:12:55 +0530

    Time: 10 ms (74.10%), Space: 42.8 MB (98.67%) - LeetHub

---
+++ 1226-the-dining-philosophers/1226-the-dining-philosophers.java
@@ -0,0 +1,35 @@
+class DiningPhilosophers {
+    Semaphore left,right;
+    Semaphore[] philosopherS;
+    Phaser phaser;
+    public DiningPhilosophers() {
+        left=new Semaphore(1);
+        right=new Semaphore(1);
+        phaser=new Phaser(1);
+        philosopherS=new Semaphore[]{new Semaphore(1),new Semaphore(1),new Semaphore(1),new Semaphore(1),new Semaphore(1)};
+        
+    }
+
+    // call the run() method of any runnable to execute its code
+    public synchronized void wantsToEat(int philosopher,
+                           Runnable pickLeftFork,
+                           Runnable pickRightFork,
+                           Runnable eat,
+                           Runnable putLeftFork,
+                           Runnable putRightFork) throws InterruptedException {
+        
+        philosopherS[philosopher].acquire();
+        left.acquire();
+        pickLeftFork.run();
+        right.acquire();
+        pickRightFork.run();
+       // phaser.arriveAndAwaitAdvance();
+        eat.run();
+        putLeftFork.run();
+        left.release();
+        putRightFork.run();
+        right.release();
+        philosopherS[philosopher].release();
+        
+    }
+}
