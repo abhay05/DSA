8024bbfe615f67ab7dc4a0c9fb24f22bc8d396f9
abhay05 <abhaypsc.5@gmail.com>
2022-04-23 13:34:26 +0530

    Time: 3 ms (79.23%), Space: 42.5 MB (80.07%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.java
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.java
@@ -1,13 +1,45 @@
 public class Codec {
+    
+    Map<String,String>mp=new HashMap<String,String>();
+    String base62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+    
     // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
-        return longUrl;
+        
+        // calculate md5 hash
+        // MessageDigest md= MessageDigest.getInstance("MD5");
+        // byte[] messageDigest=md.digest(longUrl.getBytes());
+        // BigInteger big=new BigInteger(1,messageDigest);
+        // String hashtext = big.toString(16);
+        //     while (hashtext.length() < 32) {
+        //         hashtext = "0" + hashtext;
+        //     }
+        //     //return hashtext;
+        // System.out.println(hashtext);
+        
+        // encode md5 hash with base62;
+        String hashtext=longUrl;
+        StringBuilder sb=new StringBuilder();
+        for(char e:hashtext.toCharArray()){
+            int ind=((int)e % 62);
+            sb.append(base62.charAt(ind));
+            
+        }
+        
+        while (sb.length() < 7) {
+        sb.append(0);
+        }
+        
+        String shortUrl=sb.substring(0,7);
+        String ans="http://tinyurl.com/"+shortUrl;
+        mp.put(ans,longUrl);
+        return ans;
     }
 
     // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-        return shortUrl;
+        return mp.get(shortUrl);
     }
 }
 
