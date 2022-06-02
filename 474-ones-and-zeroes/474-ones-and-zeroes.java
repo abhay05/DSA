8285f77b15dbf2f40b88a02663fc31f3841cb00b
abhay05 <abhaypsc.5@gmail.com>
2022-06-02 19:22:41 +0530

    Time: 46 ms (54.19%), Space: 44.6 MB (44.89%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.java
+++ 474-ones-and-zeroes/474-ones-and-zeroes.java
@@ -2,9 +2,8 @@ class Solution {
     public int findMaxForm(String[] strs, int m, int n) {
         int[][] dp=new int[101][101];
         dp[0][0]=0;
-        List<Pair<Integer,Integer>>list=new ArrayList<Pair<Integer,Integer>>();
-        for(int i=0;i<strs.length;i++){
-            String s=strs[i];
+        for(int k=0;k<strs.length;k++){
+            String s=strs[k];
             int cnt0=0;
             int cnt1=0;
             for(int j=0;j<s.length();j++){
@@ -14,14 +13,10 @@ class Solution {
                     cnt1++;
                 }
             }
-            list.add(new Pair(cnt0,cnt1));
-        }
-        for(int k=0;k<list.size();k++){
-            Pair<Integer,Integer>p=list.get(k);
-        for(int i=0;i<=m-p.getKey();i++){
-            for(int j=0;j<=n-p.getValue();j++){
-                if(dp[i][j]<dp[i+p.getKey()][j+p.getValue()]+1){
-                            dp[i][j]=dp[i+p.getKey()][j+p.getValue()]+1;
+        for(int i=0;i<=m-cnt0;i++){
+            for(int j=0;j<=n-cnt1;j++){
+                if(dp[i][j]<dp[i+cnt0][j+cnt1]+1){
+                            dp[i][j]=dp[i+cnt0][j+cnt1]+1;
                         }
                     }
             }
