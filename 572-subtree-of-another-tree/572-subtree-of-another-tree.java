9a7cc8081f0cbbd7a0bb18d93a3e35f38930c053
abhay05 <abhaypsc.5@gmail.com>
2022-06-08 20:46:57 +0530

    Time: 23 ms (5.01%), Space: 49.4 MB (5.10%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.java
@@ -1,32 +1,38 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    
-    public boolean match(TreeNode root,TreeNode subRoot){
-        if(root==null && subRoot==null)return true;
-        if(root==null || subRoot==null)return false;
-        if(root.val!=subRoot.val)return false;
-        return (match(root.left,subRoot.left) && match(root.right,subRoot.right));
-    }
-    
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(root==null && subRoot==null)return true;
-        if(root==null || subRoot==null)return false;
-        boolean matchBool=false;
-        if(root.val==subRoot.val){matchBool= match(root,subRoot);}
-        return (matchBool | isSubtree(root.left,subRoot) | isSubtree(root.right,subRoot));
+        return kmp(serialize(root), serialize(subRoot));
+    }
+    boolean kmp(String s, String p) { // Check if s contains p?
+        int[] lps = getLPS(p);
+        int n = s.length(), m = p.length();
+        for (int i = 0, j = 0; i < n; ++i) {
+            while (s.charAt(i) != p.charAt(j) && j > 0) j = lps[j-1];
+            if (s.charAt(i) == p.charAt(j)) j++;
+            if (j == m) return true;
+        }
+        return false;
+    }
+    int[] getLPS(String p) {
+        int m = p.length();
+        int[] lps = new int[m];
+        for (int i = 1, j = 0; i < m; ++i) {
+            while (p.charAt(i) != p.charAt(j) && j > 0) j = lps[j-1];
+            if (p.charAt(i) == p.charAt(j)) lps[i] = ++j;
+        }
+        return lps;
+    }
+    String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        serialize(root, sb);
+        return sb.toString();
+    }
+    void serialize(TreeNode root, StringBuilder sb) {
+        if (root == null) {
+            sb.append(",#");
+        } else {
+            sb.append("," + root.val);
+            serialize(root.left, sb);
+            serialize(root.right, sb);
+        }
     }
 }
