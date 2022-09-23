e3f7be8cbaa7d276de58a9bba203a27ce1681707
abhay05 <abhaypsc.5@gmail.com>
2022-09-23 23:27:10 +0530

    Time: 406 ms (42.12%), Space: 96 MB (92.18%) - LeetHub

--- 2333-minimum-sum-of-squared-difference/2333-minimum-sum-of-squared-difference.cpp
+++ 2333-minimum-sum-of-squared-difference/2333-minimum-sum-of-squared-difference.cpp
@@ -1,71 +1,37 @@
 class Solution {
 public:
     long long minSumSquareDiff(vector<int>& nums1, vector<int>& nums2, int k1, int k2) {
-        int t=k1+k2;
-        int n=nums1.size();
-        map<int,int>mp;
-        for(int i=0;i<n;i++){
-            mp[abs(nums1[i]-nums2[i])]++;
-        }
         long long ans=0;
-        priority_queue<pair<int,int>>pq;
-        for(auto& e:mp){
-            if(e.first>0){
-            pq.push({e.first,e.second});}
+        int t=k1+k2;
+        int arr[100001];
+        int n=nums1.size();
+        for(int i=0;i<100001;i++){
+            arr[i]=0;
         }
-        while(pq.size()>0 && t>0){
-            pair<int,int>p=pq.top();
-            pq.pop();
-            int val=p.first;
-            int cnt=p.second;
-           // cout<<"ff "<<val<<' '<<cnt<<' '<<t<<'\n';
-            if(cnt>=t){
-                cnt-=t;
-                int cnt1=t;
-                int val1=val-1;
-                if(val1>0){
-                    if(pq.size()>0){
-                    pair<int,int>p1=pq.top();
-                    if(p1.first==val1){
-                        pq.pop();
-                        pq.push({val1,p1.second+cnt1});
-                    }
-                        else{
-                             pq.push({val1,cnt1});
-                        }
-                }else{
-                pq.push({val1,cnt1});
-                    }
-                }
-                pq.push({val,cnt});
-                t=0;
-            }else{
-                t-=cnt;
-                val--;
-                if(val>0){
-                if(pq.size()>0){
-                    pair<int,int>p1=pq.top();
-                    if(p1.first==val){
-                        pq.pop();
-                        pq.push({val,p1.second+cnt});
-                    }else{
-                        pq.push({val,cnt});
-                    }
-                }else{    
-                pq.push({val,cnt});
-                }
-                }
-            }
+        for(int i=0;i<n;i++){
+            arr[abs(nums1[i]-nums2[i])]++;
         }
         
-        while(pq.size()>0){
-            pair<int,int>p=pq.top();
-            pq.pop();
-            int val=p.first;
-            int cnt=p.second;
-           // cout<<val<<' '<<cnt<<'\n';
-            ans+=((long long)val*(long long)val*(long long)cnt);
+        for(int i=100000;i>0;i--){
+            if(t<=0)break;
+            int val=min(t,arr[i]);
+            if(t>val){
+                t-=val;
+                arr[i-1]+=arr[i];
+                arr[i]=0;
+            }else{
+                arr[i-1]+=t;
+                arr[i]-=t;
+                t=0;
+            }
+            
         }
+        
+        for(int i=0;i<=100000;i++){
+            ans+=(long long)i*(long long)i*(long long)arr[i];
+        }
+        
         return ans;
+        
     }
 };
