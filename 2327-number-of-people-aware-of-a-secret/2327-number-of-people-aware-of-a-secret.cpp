4a003922e12c905e88b4e95ca4f9d39c7f0f82ee
abhay05 <abhaypsc.5@gmail.com>
2022-09-25 11:02:55 +0530

    Time: 4 ms (75.84%), Space: 5.9 MB (97.51%) - LeetHub

--- 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.cpp
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.cpp
@@ -2,43 +2,36 @@ class Solution {
 public:
     int MOD=1000000007;
     int peopleAwareOfSecret(int n, int delay, int forget) {
-        long long arr[n];
-        long long ans[n];
+        int arr[n];
+        int ans[n];
         for(int i=0;i<n;i++){
-            arr[i]=(long long)0;
-            ans[i]=(long long)0;
+            arr[i]=0;
+            ans[i]=0;
         }
         int cnt=1;
-        arr[0]=(long long)0;
-        ans[0]=(long long)1;
+        arr[0]=0;
+        ans[0]=1;
         if(delay<n){
-            arr[delay]+=(long long)1;
+            arr[delay]+=1;
             }
             if(forget<n){
-            arr[forget]-=(long long)1;
-                ans[forget]=(long long)-1;
+            arr[forget]-=1;
+                ans[forget]=-1;
             }
         for(int i=1;i<n;i++){
-                arr[i]=(arr[i]+arr[i-1])%MOD;
+            
+                arr[i]=((arr[i]+MOD)%MOD+(arr[i-1]+MOD)%MOD)%MOD;
+            ans[i]=(ans[i]+(arr[i]+ans[i-1])%MOD)%MOD;
+            if(ans[i]<0){ans[i]=(ans[i]+MOD)%MOD;}
             if(i+delay<n){
-            arr[i+delay]=(arr[i+delay]+arr[i]);
-                
+            arr[i+delay]+=arr[i];
             }
             if(i+forget<n){
-            arr[i+forget]=(arr[i+forget]-arr[i]);
-            }
-        }
-        for(int i=max(0,n-forget)+1;i<n;i++){
-            arr[i]=(arr[i]+arr[i-1])%MOD;
+            arr[i+forget]-=arr[i];
+                ans[i+forget]-=arr[i];
+            } 
         }
         
-        if(forget>=n){
-            arr[n-1]=(arr[n-1]+1)%MOD;
-            
-        }
-        if(arr[n-1]<0){
-            arr[n-1]=(arr[n-1]+MOD)%MOD;
-        }
-        return (int)arr[n-1];
+        return ans[n-1];
     }
 };
