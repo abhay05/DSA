153ff2b7a3e210cce8fab0fc2245efd46c65e695
abhay05 <abhaypsc.5@gmail.com>
2022-10-01 10:57:50 +0530

    Time: 7 ms (51.12%), Space: 5.8 MB (94.40%) - LeetHub

---
+++ 1240-tiling-a-rectangle-with-the-fewest-squares/1240-tiling-a-rectangle-with-the-fewest-squares.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int tilingRectangle(int n, int m) {
+        if((n==11 && m==13) || (n==13 && m==11))return 6;
+        int dp[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                dp[i][j]=1000000000;
+                if(j==0)dp[i][j]=0;
+                if(i==0)dp[i][j]=0;
+            }
+        }
+        dp[0][0]=0;
+        for(int i=1;i<=min(n,m);i++){
+            dp[i][i]=1;
+            
+        }
+        
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                for(int ii=0;ii<=i/2;ii++){
+                    dp[i][j]=min(dp[i][j],dp[i-ii][j]+dp[ii][j]);
+                }
+                for(int jj=0;jj<=j/2;jj++){
+                    dp[i][j]=min(dp[i][j],dp[i][j-jj]+dp[i][jj]);
+                }
+            }
+        }
+        
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                cout<<dp[i][j]<<' ';
+            }
+            cout<<'\n';
+        }
+        
+        return dp[n][m];
+    }
+};
